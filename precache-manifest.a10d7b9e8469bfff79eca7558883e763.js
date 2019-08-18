b10ef58edbcb1044d0ced5e3cbb79a7963c4008d
Nilesh Jain <nileshjain166@gmail.com>
2019-08-18 15:51:09 +0530

    Updates

---
+++ precache-manifest.a10d7b9e8469bfff79eca7558883e763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e2f85a343fecd7041fb59609281e44",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eeb067b3115788f75600",
+    "url": "/monsters-rolodex/static/css/main.34aedb78.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monsters-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "eeb067b3115788f75600",
+    "url": "/monsters-rolodex/static/js/main.ae2ec94a.chunk.js"
+  },
+  {
+    "revision": "c7b84e328d40a43989ce",
+    "url": "/monsters-rolodex/static/js/runtime~main.45a8f4a3.js"
+  }
+]);
